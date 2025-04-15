fec81752dbf760ead517cfcf5db587ae05020da5
NajeebUrRehmanRaja <najeebraja22.2@gmail.com>
2025-04-15 17:31:51 +0500

    refactor: update App component structure and change DaisyUI theme to dark

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="text-3xl text-gray-200">
-      <button className="btn">Default</button>
-    </div>
+    <>
+      
+    </>
   );
 }
 
