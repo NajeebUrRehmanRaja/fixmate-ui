8245ac3bb9c877c2c72dadb8e06ec6a04ba77080
NajeebUrRehmanRaja <najeebraja22.2@gmail.com>
2025-04-18 11:49:33 +0500

    feat: enhance Home page layout and add multi-language support feature with updated button component

--- FrontEnd/src/components/ButtonComponents.jsx
+++ FrontEnd/src/components/ButtonComponents.jsx
@@ -1,7 +1,7 @@
 const HandleButton = ({ children, color, ...props }) => {
   return (
     <button
-      className={`px-4 py-2 rounded text-white ${
+      className={`px-4 py-2 rounded text-white cursor-pointer ${
         color === "blue" ? "bg-blue-500 hover:bg-blue-600" : ""
       } ${color === "green" ? "bg-green-500 hover:bg-green-600" : ""}`}
       {...props}
--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Handlebutton from "../components/ButtonComponents";
 const Home = () => {
   return (
-    <div className="pt-20">
-      <div>
-        <h1 className="text-4xl font-bold text-center">Welcome to the Home Page</h1>
-        <p className="text-center mt-4">This is the home page of our application.</p>
+    <div className="flex flex-col items-center justify-center pt-50">
+      <div className="flex flex-col items-center justify-center space-y-2 ">
+        <h6 className="px-3 py-1 border rounded-full" >
+          New Feature: Multi-language Support
+        </h6>
+        <h1 className="text-3xl flex flex-col justify-center items-center font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">
+          AI-Powered Code Review <br />
+          <span className="bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent h-20">
+            Detect Bugs Before They Ship
+          </span>
+        </h1>
       </div>
+       <p className="mx-auto flex flex-col justify-center items-center max-w-[700px] text-muted-foreground md:text-xl text-gray-400">
+          CodeScribe helps developers write better code with instant automated <span></span>
+          code reviews, bug detection, and smart suggestions.
+        </p>
+        <Link to={"/getstarted"}>
+          <button 
+          children="Get Started"
+          className="mt-4 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-bold py-2 px-4 rounded-full hover:shadow-lg hover:shadow-blue-500/50 transition duration-300 ease-in-out"
+          icon="arrow-right cursor-pointer"
+           />
+        </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
