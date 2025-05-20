a7bc7e9536dac89018e90e9a8c64ffb1145242d2
NajeebUrRehmanRaja <najeebraja22.2@gmail.com>
2025-05-20 14:22:51 +0500

    fix: correct schema definition for User model and ensure proper field types

--- BackEnd/src/Models/User.Model.js
+++ BackEnd/src/Models/User.Model.js
@@ -1,14 +1,17 @@
 import mongoose from 'mongoose'
 
-const userSchema = new schema({
-  firstname: { type: String, require: true },
-  lastname: { type: String, require: true },
-  email: { type: String, require: true },
-  clerk: {  },
-  imgurl: { },
-  credit: { default: 5, },
-  stripeid: {},
-}, { timestamps: true}
+const userSchema = new schema(
+  {
+    firstname: { type: String, require: true },
+    lastname: { type: String, require: true },
+    email: { type: String, require: true },
+    clerk: { type: String },
+    imgurl: { type: String },
+    credit: { type: Number, default: 5 },
+    stripeid: { type: String },
+    code: { type: String },
+  },
+  { timestamps: true }
 );
 
 export const User = mongoose.model("User", userSchema);
